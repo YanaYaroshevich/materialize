a853922cb2c0c2b75125153ad8750e975807325c
yyaroshevich <yyaroshevich@exadel.com>
2019-01-04 17:01:49 +0300

    Fix property name

--- js/dropdown.js
+++ js/dropdown.js
@@ -17,7 +17,7 @@
       belowOrigin: false,
       alignment: 'left',
       stopPropagation: false,
-      container: window
+      scrollContainer: window
     };
 
     // Open dropdown.
@@ -61,8 +61,8 @@
           curr_options.alignment = origin.data('alignment');
         if (origin.data('stoppropagation') !== undefined)
           curr_options.stopPropagation = origin.data('stoppropagation');
-        if (origin.data('container') !== undefined)
-          curr_options.container = origin.data('container');
+        if (origin.data('scrollcontainer') !== undefined)
+          curr_options.scrollContainer = origin.data('scrollcontainer');
       }
 
       updateOptions();
@@ -98,11 +98,11 @@
         }
 
         // Offscreen detection
-        var containerInnerHeight = $(curr_options.container).innerHeight;
+        var containerInnerHeight = $(curr_options.scrollContainer).innerHeight;
         var windowHeight = typeof containerInnerHeight === 'function' ? containerInnerHeight() : containerInnerHeight;
         var originHeight = origin.innerHeight();
         var offsetLeft = origin.offset().left;
-        var offsetTop = origin.offset().top - $(curr_options.container).scrollTop();
+        var offsetTop = origin.offset().top - $(curr_options.scrollContainer).scrollTop();
         var currAlignment = curr_options.alignment;
         var gutterSpacing = 0;
         var leftPosition = 0;
